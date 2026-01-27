bc9a9b17f52a7f3d117cd44bc293250af2409db0
ShivaBoya <shivasiddu80@gmail.com>
2026-01-27 17:46:29 +0530

    Updated

--- src/services/FirebaseConfig.js
+++ src/services/FirebaseConfig.js
@@ -34,7 +34,6 @@
 //   // appId: "1:501919675960:web:85868df5ee8ef1a77858cd",
 //   // measurementId: "G-NFVZBFX2QE"
 
-
 //   apiKey: "AIzaSyD03YRikMYZnnncwJWyjDf2wVFer-vukqU",
 //   authDomain: "telegram-e84b9.firebaseapp.com",
 //   projectId: "telegram-e84b9",
@@ -43,29 +42,25 @@
 //   appId: "1:18200002246:web:df225386da8cf6d53861c7",
 //   measurementId: "G-90BTGM8R0N"
 
-
 // }
-
 
 // const app = initializeApp(firebaseConfig)
 // const database = getDatabase(app)
 
-
-
 // export { app, database }
 
-
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 const firebaseConfig = {
-  apiKey: "AIzaSyAr7pLAFvWzMtdaux2CooAOgLqtw9JOZIs",
-    authDomain: "telegram-mini-applicatio-b17a1.firebaseapp.com",
-    databaseURL: "https://telegram-mini-applicatio-b17a1-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "telegram-mini-applicatio-b17a1",
-    storageBucket: "telegram-mini-applicatio-b17a1.firebasestorage.app",
-    messagingSenderId: "66491108378",
-    appId: "1:66491108378:web:dc3b649889117e17f4d5a3",
-    measurementId: "G-PDL9LQKX8J"
+  apiKey: "AIzaSyD03YRikMYZnnncwJWyjDf2wVFer-vukqU",
+  authDomain: "telegram-e84b9.firebaseapp.com",
+  databaseURL:
+    "https://telegram-e84b9-default-rtdb.asia-southeast1.firebasedatabase.app", // ✅ FIXED: region + no spaces
+  projectId: "telegram-e84b9",
+  storageBucket: "telegram-e84b9.firebasestorage.app",
+  messagingSenderId: "18200002246",
+  appId: "1:18200002246:web:df225386da8cf6d53861c7",
+  measurementId: "G-90BTGM8R0N",
 };
 
 // Initialize Firebase
